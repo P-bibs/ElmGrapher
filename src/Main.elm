e9c53c881ea561b958f4f712d0fee4101a6ef044
Paul Biberstein <biberstein.paul@gmail.com>
2019-02-26 11:22:26 -0500

    Added architecture for basic animation

--- src/Main.elm
+++ src/Main.elm
@@ -2,11 +2,12 @@ import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput)
-import Svg 
+import Svg
 import Svg.Attributes  exposing (x1, y1, x2, y2)
+import Animation exposing (px)
 
 graphWidth : Float
-graphWidth = 1000.0 
+graphWidth = 1000.0
 
 graphHeight : Float
 graphHeight = 1000.0
@@ -17,6 +18,15 @@ intervalSize = 1
 radius : Float
 radius = 0.5
 
+defaultLineAttributes : List (Svg.Attribute msg)
+defaultLineAttributes =
+  [
+    Svg.Attributes.stroke "#D80707" ,
+    Svg.Attributes.strokeWidth "1",
+    Svg.Attributes.strokeMiterlimit "10",
+    Svg.Attributes.fill "none"
+  ]
+
 type alias AbstractCircle =
   {
       cx : Float
@@ -26,7 +36,7 @@ type alias AbstractCircle =
 
 
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+  Browser.element { init = init, update = update, view = view, subscriptions = subscriptions }
 
 
 -- MODEL
@@ -34,14 +44,20 @@ type alias Model =
   { innerScalar : Float
   , outerScalar : Float
   , func : (Float -> Float)
+  , style : Animation.State
   }
 
-init : Model
-init =
-  { innerScalar = 0.0
-  , outerScalar = 0.0
-  , func = (sin)
-  }
+init : () -> (Model, Cmd Msg)
+init _ =
+  let 
+    model = {
+      innerScalar = 0.0,
+      outerScalar = 0.0,
+      func = sin,
+      style = (Animation.style [ Animation.left (px 0.0), Animation.opacity 1.0])
+      }
+  in
+    (model, Cmd.none)
 
 
 -- UPDATE
@@ -49,39 +65,63 @@ type Msg
   = ChangeInner String
   | ChangeOuter String
   | ChangeFunc String
+  | FadeLines
+  | Animate Animation.Msg
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     ChangeInner changeData ->
-      {
+      ({
         model | innerScalar =
           case String.toFloat changeData of
             Nothing ->
               0
             Just val ->
               val
-      }
+      },
+      Cmd.none)
 
     ChangeOuter changeData ->
-      {
+      ({
         model | outerScalar =
           case String.toFloat changeData of
             Nothing ->
               0
             Just val ->
               val
-      }
+      },
+      Cmd.none)
 
     ChangeFunc changeData ->
       if changeData == "sin" then
-        {model | func = sin}
+        ({model | func = sin}, Cmd.none)
       else if changeData == "cos" then
-        {model | func = cos}
+        ({model | func = cos}, Cmd.none)
       else if changeData == "tan" then
-        {model | func = tan}
+        ({model | func = tan}, Cmd.none)
       else
-        model
+        (model, Cmd.none)
+
+    FadeLines ->
+      ( { model
+        | style =
+            Animation.interrupt [Animation.to [Animation.opacity 0]] model.style
+      }
+      , Cmd.none
+      )
+
+    Animate animMsg ->
+      ({ model
+        | style = Animation.update animMsg model.style
+      },
+      Cmd.none)
+
+-- SUPSCRIPTIONS
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Animation.subscription Animate [ model.style ]
+
 
 -- VIEW
 view : Model -> Html Msg
@@ -94,7 +134,7 @@ view model =
         div [] [Html.text ("Inner: " ++ String.fromFloat model.innerScalar)],
         Html.input [placeholder "", value (String.fromFloat model.innerScalar), onInput ChangeInner] [],
 
-        div [] [Html.text ("Outer: " ++ String.fromFloat model.outerScalar)],
+        div (Animation.render model.style) [Html.text ("Outer: " ++ String.fromFloat model.outerScalar)],
         Html.input [placeholder "", value (String.fromFloat model.outerScalar), onInput ChangeOuter] [],
 
         div [] [],
@@ -102,6 +142,10 @@ view model =
         button [onClick (ChangeFunc "sin")] [Html.text "sin"],
         button [onClick (ChangeFunc "cos")] [Html.text "cos"],
         button [onClick (ChangeFunc "tan")] [Html.text "tan"],
+
+        div [] [],
+
+        button [onClick FadeLines ] [text "Fade Out Lines"],
 
         div [] [Html.text (String.fromFloat model.outerScalar ++ "(sin/cos/tan)" ++ "(" ++ String.fromFloat model.innerScalar ++ "x)")]
       ],
@@ -139,27 +183,19 @@ makeLinesFromCircles circleList =
     [] -> []
     [a] -> []
     [a,b] ->
-      [Svg.line [
+      [Svg.line ([
         x1 (String.fromFloat a.cx),
         y1 (String.fromFloat a.cy),
         x2 (String.fromFloat b.cx),
-        y2 (String.fromFloat b.cy),
-        Svg.Attributes.stroke "#D80707" ,
-        Svg.Attributes.strokeWidth "1",
-        Svg.Attributes.strokeMiterlimit "10",
-        Svg.Attributes.fill "none"
-        ] []
+        y2 (String.fromFloat b.cy)
+        ]++defaultLineAttributes) []
       ]
     a::b::_ ->
-      (Svg.line [
+      (Svg.line ([
         x1 (String.fromFloat a.cx),
         y1 (String.fromFloat a.cy),
         x2 (String.fromFloat b.cx),
-        y2 (String.fromFloat b.cy),
-        Svg.Attributes.stroke "#D80707" ,
-        Svg.Attributes.strokeWidth "1",
-        Svg.Attributes.strokeMiterlimit "10",
-        Svg.Attributes.fill "none"
-        ] []
+        y2 (String.fromFloat b.cy)
+        ]++defaultLineAttributes) []
       ) :: makeLinesFromCircles (List.drop 1 circleList)
   
